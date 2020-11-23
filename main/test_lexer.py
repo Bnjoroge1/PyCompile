7a0798ccf16b70b3274d7d8e79c94705b36b954f
Bill Njoroge <williamsriunge@gmail.com>
2020-11-23 22:33:42 +0300

    added readme and tests

--- main/test_lexer.py
+++ main/test_lexer.py
@@ -1,13 +1,22 @@
-from lexer import Lexer
-
+from lexer import Lexer, Token, TokenType
 
 def main():
-     input = 'tesererer erev efeeeeeeeeeeeee'
-     lexer_test = Lexer(input)
+     
+     def test_look_ahead_char():
+          input = 'Let a = 23'
+          lexer_test = Lexer(input)
 
-     while lexer_test.look_ahead_next_char() != '\0':
-          next_char = lexer_test.current_char
-          print(next_char)
-          lexer_test.process_next_char()
+          while lexer_test.look_ahead_next_char() != '\0':
+               next_char = lexer_test.current_char
+               print(next_char)
+               lexer_test.process_next_char()
+def test_arithmetic_operators():
+     input = '+-*/'
+     lexer_arithmetic = Lexer(input)
+     token = lexer_arithmetic.get_next_token()
+     while token.type != '' TokenType.EOF:
+          print(token.type)
+          token_confirm =  lexer_arithmetic.get_next_token()
 
-main()
+if __name__ == "__main__":
+         main()
